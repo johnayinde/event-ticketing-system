6399a7a1a0224e33d44217af8fe96458b00ce3e2
Ayinde John <lolaayinde@gmail.com>
2025-02-09 18:04:12 +0100

    increased rate limit for test

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const rateLimit = require("express-rate-limit");
 
 /**
- *Per IP address => 20 requests per minute
+ *Per IP address => 15 requests per minute
  */
+
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 20,
+  max: process.env.NODE_ENV === "test" ? 50 : 15,
   message: {
     error: "Too many requests",
     message: "Please try again later",
